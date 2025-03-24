095203c0f052087910c90d52df3d14502d611424
nekolay52 <shishaev090@gmail.com>
2025-03-24 20:12:05 +0400

    формирование док

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from docx import Document
 from docx.shared import Pt
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 def dngffdfkg(doc):
@@ -18,27 +19,49 @@ response.raise_for_status()
 
 soup = BeautifulSoup(response.text, features="html.parser")
 some_headline = soup.find('h1', class_="firstHeading mw-first-heading")
-divan = soup.find('div', class_="mw-content-ltr mw-parser-output")
-tosh_divan = divan.find_all(['p', "h1", "h2", "h3", "h4", "h5"])
+ocnovnoy = soup.find('div', class_="mw-content-ltr mw-parser-output")
+tosh_divan = ocnovnoy.find_all(['p', "h1", "h2", "h3", "h4", "h5", "h6", "img"])
+tosh_divan = [some_headline] + tosh_divan
 
-# print(some_headline.text)
-# for i in tosh_divan:
-#     print(i.text)
+#удоление пустЮ разд-ов
+
+for i in range(len(tosh_divan) - 1, -1, -1):
+    if "<h" not in str(tosh_divan[i]):
+        w = i
+        break
+
+qyqy = tosh_divan[:w + 1]
 
 doc = Document()
 
 doc = dngffdfkg(doc)
 
-for teg in tosh_divan:
-    if "<p>" in str(teg):
-        s = doc.add_paragraph(teg.text)
-        s.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
 
-# head = doc.add_heading('Добавление заголовка документа', level=1)
-# head2 = doc.add_heading(' заголовка документа', level=2)
-# head.alignment = WD_ALIGN_PARAGRAPH.CENTER
-# head2.alignment = WD_ALIGN_PARAGRAPH.CENTER
+for teg in qyqy:
+    if "<p>" in str(teg):
+        w = re.sub(r'\[.*?\]', '', teg.text)
+        s = doc.add_paragraph(w)
+        s.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+    if "<h1" in str(teg):
+        head = doc.add_heading(teg.text, level=1)
+        head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    if "<h2" in str(teg):
+        head = doc.add_heading(teg.text, level=2)
+        head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    if "<h3" in str(teg):
+        head = doc.add_heading(teg.text, level=3)
+        head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    if "<h4" in str(teg):
+        head = doc.add_heading(teg.text, level=4)
+        head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    if "<h5" in str(teg):
+        head = doc.add_heading(teg.text, level=5)
+        head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    if "<h6" in str(teg):
+        head = doc.add_heading(teg.text, level=6)
+        head.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    if "<img" in str(teg):
+       print("<img")
 
 doc.save('test.docx')
 
-print(tosh_divan[0])
